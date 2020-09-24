f06d89469a2a92b0cfa474efdb4f834707b66772
barackmaund1 <pmaundu49@gmail.co>
2020-09-24 13:15:37 +0300

    build the react Link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import './Home.css';
+import {link, Link} from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div className='home'>
-           <h1>This is home page</h1>
-           <div className='home__header'>
-               <div className='home__headerLeft'>
-                {/* Link*/}
-                {/*Link */}
-                </div>
-                <div className='home__headerRight'>
-                 {/*Link */}
-                 {/*Link */}
-                 {/* Icon*/}
-                 {/*Avatar */}
-               </div>
-           </div>
-           <div className='home__body'>
-
-           </div>
+      <div className="home">
+        <h1>This is home page</h1>
+        <div className="home__header">
+          <div className="home__headerLeft">
+            {/* Link*/}
+            <Link to="/about">About</Link>
+            <Link to="/store">Store</Link>
+            
+          </div>
+          <div className="home__headerRight">
+            {/*Link */}
+            <Link to="/gmail">Gmail</Link>
+            <Link to="/images">Images</Link>
+            
+            {/* Icon*/}
+            {/*Avatar */}
+          </div>
         </div>
-    )
+        <div className="home__body"></div>
+      </div>
+    );
 }
 
 export default Home
