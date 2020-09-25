3aafe90aa1d008a2a686d8054df266f2f399ed9c
barackmaund1 <pmaundu49@gmail.co>
2020-09-25 04:32:44 +0300

    build working google search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic'
 import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
-const Search = () => {
+const Search = ({hideButtons =false}) => {
+    const [input,setInput]=useState('');
+    const history =useHistory();
+
     const search = e =>{
       e.preventDefault()
+
+      console.log('You hit the search button >>>',input)
+      // do something with input...come back and fix
+
+      history.push('/search')
     }
 
     return (
-        <div className='search'>
-             <div className='search__input'>
-                <SearchIcon className='search__inputIcon'/>
-                <input placeholder='Search Google or type a URL'/>
-                <MicIcon/>
-            </div>
-            <div className='search__button'>
-               <Button onClick={search} variant='outlined'>Google Search</Button>
-               <Button variant='outlined'>i'm feeling lucky</Button>
-            </div>
+      <form className="search">
+        <div className="search__input">
+          <SearchIcon className="search__inputIcon" />
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Search Google or type a URL"
+          />
+          <MicIcon />
         </div>
-    )
+
+        {!hideButtons ? (
+          <div className="search__button">
+            <Button
+              
+              type="submit"
+              onClick={search}
+              variant="outlined"
+            >
+              Google Search
+            </Button>
+            <Button variant="outlined">
+              i'm feeling lucky
+            </Button>
+          </div>
+        ) : (
+          <div className="search__button">
+            <Button
+              className="search__buttonHiddden"
+              type="submit"
+              onClick={search}
+              variant="outlined"
+            >
+              Google Search
+            </Button>
+            <Button className="search__buttonHiddden" variant="outlined">
+              i'm feeling lucky
+            </Button>
+          </div>
+        )}
+      </form>
+    );
 }
 
 export default Search
