62c4d49ba62cdd7d9977360b7e2208a490924885
barackmaund1 <pmaundu49@gmail.co>
2020-09-28 12:33:11 +0300

    Updates

---
+++ precache-manifest.2f960283ecf0e4cafccd4787c4f3ba7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648c11da66d0361f2a07f0ea3627517c",
+    "url": "/google-clone/index.html"
+  },
+  {
+    "revision": "de4f19d256c292ffe206",
+    "url": "/google-clone/static/css/main.e1ae7dc7.chunk.css"
+  },
+  {
+    "revision": "0440325a39e3881e13a3",
+    "url": "/google-clone/static/js/2.40095e15.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google-clone/static/js/2.40095e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4f19d256c292ffe206",
+    "url": "/google-clone/static/js/main.bf97cb9a.chunk.js"
+  },
+  {
+    "revision": "53e6f9d7fcbadc32d4de",
+    "url": "/google-clone/static/js/runtime-main.20aa3e5d.js"
+  }
+]);
